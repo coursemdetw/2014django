73b8c1e56915f869e0c0f0a1427b1433d84b89df
chiaming yen <chiamingyen@gmail.com>
2014-06-01 00:00:27 +0800

    initial add django project

---
+++ wsgi.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import imp
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.append(os.path.join('wsgi', 'openshift'))
+
+#
+# Below for running on localhost
+#
+if __name__ == '__main__':
+    ip   = 'localhost'
+    port = 8051
+    zapp = imp.load_source('application', 'wsgi/application')
+
+    from wsgiref.simple_server import make_server
+    httpd = make_server(ip, port, zapp.application)
+    print("Django is running on:", ip, port)
+    httpd.serve_forever()
