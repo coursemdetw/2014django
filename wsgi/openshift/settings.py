ad75929f0f64bdfb5e77b6d1774216d64d104a5c
chiaming yen <chiamingyen@gmail.com>
2014-06-02 00:28:42 +0800

    try to use https on OpenShift

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -83,8 +83,6 @@ if ON_OPENSHIFT:
             'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'db.sqlite3'),
         }
     }
-    # try to use https for OpenShift
-    os.environ['wsgi.url_scheme'] = 'https'
 else:
     DATABASES = {
         'default': {
